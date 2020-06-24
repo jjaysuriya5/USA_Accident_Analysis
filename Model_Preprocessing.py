c6563deada178932b202cbc38e2aa3354659e365
jjaysuriya5 <53468162+jjaysuriya5@users.noreply.github.com>
2020-06-24 20:18:29 +0530

    Update Model_Preprocessing.py

--- Model_Preprocessing.py
+++ Model_Preprocessing.py
@@ -145,7 +145,7 @@ def train_pipe():
     test_size = 0.2
     x_train , x_test , y_train , y_test = train_test_split( x , y , test_size = test_size , random_state = 0 , stratify = y )
     
-    preprocessing_pipeline.fit(x_train)
+    u_d = preprocessing_pipeline.fit(x_train)
     
     return preprocessing_pipeline
             
